3b0919b55b3022fbdee76e71625a8230b2f9004d
AshhadAbid <133577886+AshhadAbid@users.noreply.github.com>
2024-06-28 12:28:09 +0000

    conversion

---
+++ 01_basics/03_conversionofdatatypes.js
@@ -0,0 +1,22 @@
+let a = "33abc"
+let b = Number (a)
+
+console.log(b)
+
+
+
+let m = 33
+let c = String (m)
+
+console.log(typeof c)
+
+let bol = false;
+let s = Number (bol)
+
+console.log(s)
+
+
+let n = null
+let g = Number(n) // error for data 
+
+console.log(g)
