c83154bf51b72b9e17af77c423873940bc6958d5
AshhadAbid <133577886+AshhadAbid@users.noreply.github.com>
2024-06-28 11:34:33 +0000

    ecma data types

---
+++ 01_basics/02_ecma_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let v= false
+let a;// only declared variable
+let b = null // khaali
+let p = Symbol // unique in sense of figma and react component findness
+// number 
+// string
+// bigint
+console.log(typeof null)
