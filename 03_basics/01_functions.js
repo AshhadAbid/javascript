dc6d5356b13ad746834cbfcedd3a7db29142ae52
AshhadAbid <133577886+AshhadAbid@users.noreply.github.com>
2024-07-10 09:52:17 +0000

    functionwithobjectandspreadoperator

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+
+const obj = {
+    name:"ashhad",
+    surname:"abid"
+}
+
+function abcd(num1,num2,...cart){
+    return cart
+    
+}
+
+
+// console.log(abcd(1,2,3,4))
+
+function handleObject(anyObject){
+
+    return anyObject
+}
+
+// console.log(handleObject({
+//     name:"mmm",
+//     abid:"surname"
+// }))
+
