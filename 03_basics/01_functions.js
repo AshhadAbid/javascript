8651c4c28fcc8b63f12a7eb5e243a4623bc6e7c9
AshhadAbid <133577886+AshhadAbid@users.noreply.github.com>
2024-07-10 10:16:50 +0000

    Hoisting

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,3 +22,17 @@ function handleObject(anyObject){
 //     abid:"surname"
 // }))
 
+
+// +++++++++++++++++++++++++++HOISTING+++++++++++++++++++++++++++++++++++++++++++++
+console.log(addOne(1))// in this type of function declaration we can call function before initialization
+function addOne(num1){
+    return num1 +1
+}
+
+const addTwo = function(num1){
+    return num1 + 2
+}
+
+console.log(addTwo(5)) // in this type of function declaration we cannot call function before initialization
+
+
